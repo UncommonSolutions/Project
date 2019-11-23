853e386d040ee3b806d01c4fdb99f89463bd7f93
mkiefer-git <56896985+mkiefer-git@users.noreply.github.com>
2019-11-23 10:34:46 -0600

    Updated DDD and added comments to table build SQL

--- src/SQL/BuildTables.sql
+++ src/SQL/BuildTables.sql
@@ -1,3 +1,5 @@
+-- Uncommon Solutions Table Builds for HR System
+-- Initiate with cleaning up tables from database
 DROP TABLE UserGroupTable;
 DROP TABLE ResumeTable;
 DROP TABLE TrainingRecordTable;
@@ -9,7 +11,7 @@ DROP TABLE JobTable;
 DROP TABLE AccessLogTable;
 DROP TABLE UserLoginTable;
 
-
+-- Create primary table for the user account section of the database
 CREATE TABLE UserLoginTable (
     user_number     INTEGER PRIMARY KEY,
     user_name       VARCHAR(25) NOT NULL,
@@ -17,6 +19,9 @@ CREATE TABLE UserLoginTable (
     password_hash   VARCHAR(50) NOT NULL,
     last_login      DATETIME);
 
+-- Create the table for storing login attempts per user name
+-- Maintain referential integrity and wipe records if the associated user
+-- Account entry no longer exists
 CREATE TABLE AccessLogTable (
     time_stamp      DATE PRIMARY KEY,
     user_number     INTEGER NOT NULL,
@@ -25,15 +30,19 @@ CREATE TABLE AccessLogTable (
 		REFERENCES UserLoginTable(user_number)
         ON UPDATE CASCADE ON DELETE CASCADE);
     
+-- Create table for storage of job titles/descriptions
 CREATE TABLE JobTable (
     job_number              INTEGER PRIMARY KEY,
     position_name           VARCHAR(100) NOT NULL,
     position_description    VARCHAR(1000));
     
+-- Create table to store different work group names
 CREATE TABLE GroupTable (
     group_number    INTEGER PRIMARY KEY,
     group_name      VARCHAR(100) NOT NULL);
 
+-- Create table for storing contact information, used for both 
+-- Employees and emergency contacts
 CREATE TABLE ContactTable (
     contact_number  INTEGER PRIMARY KEY,
     last_name       VARCHAR(50) NOT NULL,
@@ -43,6 +52,8 @@ CREATE TABLE ContactTable (
     address         VARCHAR(200),
     email           VARCHAR(100));
 
+-- Create the primary personnel data table that ties to records in other
+-- tables. Maintain referential integrity by cascading updates
 CREATE TABLE PersonnelTable (
     employee_number             INTEGER PRIMARY KEY,
     user_number                 INTEGER NOT NULL,
@@ -63,6 +74,7 @@ CREATE TABLE PersonnelTable (
 		REFERENCES JobTable(job_number)
         ON UPDATE CASCADE);
     
+-- Create table for storing personnel record information for personnel actions
 CREATE TABLE PersonnelRecordTable (
     record_number   INTEGER PRIMARY KEY,
     record_date     DATE,
@@ -72,6 +84,7 @@ CREATE TABLE PersonnelRecordTable (
 		REFERENCES PersonnelTable(employee_number)
         ON UPDATE CASCADE);
 
+-- Create table for storing trainign record information for individual employees
 CREATE TABLE TrainingRecordTable (
     record_number   INTEGER PRIMARY KEY,
     record_date     DATE,
@@ -81,6 +94,7 @@ CREATE TABLE TrainingRecordTable (
 		REFERENCES PersonnelTable(employee_number)
         ON UPDATE CASCADE);
 
+-- Create table for storing resumes of employees stored by HR (utilizing BLOB)
 CREATE TABLE ResumeTable (
     resume_number   INTEGER PRIMARY KEY,
     resume_date     DATE,
@@ -90,6 +104,9 @@ CREATE TABLE ResumeTable (
 		REFERENCES PersonnelTable(employee_number)
         ON UPDATE CASCADE);
 
+-- Create referential relationship table to tie personnel to groups
+-- This method allows multiple users per group and multiple groups
+-- Per user.
 CREATE TABLE UserGroupTable (
     group_number    INTEGER NOT NULL,
     employee_number INTEGER NOT NULL,
