624b257b966d67f63278730b58a4806301a49ba6
Sean Mooneyham <swmooneyham@gmail.com>
2019-11-29 13:13:00 -0500

    added comments back in to schema script

--- SQL/BuildTables.sql
+++ SQL/BuildTables.sql
@@ -1,3 +1,5 @@
+-- Uncommon Solutions Table Builds for HR System
+-- Initiate with cleaning up tables from database
 DROP TABLE IF EXISTS UserGroupTable;
 DROP TABLE IF EXISTS ResumeTable;
 DROP TABLE IF EXISTS TrainingRecordTable;
@@ -9,7 +11,7 @@ DROP TABLE IF EXISTS JobTable;
 DROP TABLE IF EXISTS AccessLogTable;
 DROP TABLE IF EXISTS UserLoginTable;
 
-
+-- Create primary table for the user account section of the database
 CREATE TABLE UserLoginTable (
     user_number     INTEGER PRIMARY KEY AUTO_INCREMENT,
     user_name       VARCHAR(25) NOT NULL,
@@ -19,6 +21,9 @@ CREATE TABLE UserLoginTable (
 		CURRENT_TIMESTAMP 
 		ON UPDATE CURRENT_TIMESTAMP);
 
+-- Create the table for storing login attempts per user name
+-- Maintain referential integrity and wipe records if the associated user
+-- Account entry no longer exists
 CREATE TABLE AccessLogTable (
     time_stamp      TIMESTAMP PRIMARY KEY 
 		DEFAULT CURRENT_TIMESTAMP,
@@ -27,16 +32,20 @@ CREATE TABLE AccessLogTable (
     FOREIGN KEY (user_number)
 		REFERENCES UserLoginTable(user_number)
         ON UPDATE CASCADE ON DELETE CASCADE);
-    
+
+-- Create table for storage of job titles/descriptions
 CREATE TABLE JobTable (
     job_number              INTEGER PRIMARY KEY AUTO_INCREMENT,
     position_name           VARCHAR(100) NOT NULL,
     position_description    VARCHAR(1000));
-    
+
+-- Create table to store different work group names
 CREATE TABLE GroupTable (
     group_number    INTEGER PRIMARY KEY AUTO_INCREMENT,
     group_name      VARCHAR(100) NOT NULL);
 
+-- Create table for storing contact information, used for both
+-- Employees and emergency contacts
 CREATE TABLE ContactTable (
     contact_number  INTEGER PRIMARY KEY AUTO_INCREMENT,
     last_name       VARCHAR(50) NOT NULL,
@@ -46,6 +55,8 @@ CREATE TABLE ContactTable (
     address         VARCHAR(200),
     email           VARCHAR(100));
 
+-- Create the primary personnel data table that ties to records in other
+-- tables. Maintain referential integrity by cascading updates
 CREATE TABLE PersonnelTable (
     employee_number             INTEGER PRIMARY KEY AUTO_INCREMENT,
     user_number                 INTEGER NOT NULL,
@@ -65,7 +76,8 @@ CREATE TABLE PersonnelTable (
 	FOREIGN KEY (job_number)
 		REFERENCES JobTable(job_number)
         ON UPDATE CASCADE);
-    
+
+-- Create table for storing personnel record information for personnel actions
 CREATE TABLE PersonnelRecordTable (
     record_number   INTEGER PRIMARY KEY AUTO_INCREMENT,
     record_date     DATE,
@@ -75,6 +87,7 @@ CREATE TABLE PersonnelRecordTable (
 		REFERENCES PersonnelTable(employee_number)
         ON UPDATE CASCADE);
 
+-- Create table for storing training record information for individual employees
 CREATE TABLE TrainingRecordTable (
     record_number   INTEGER PRIMARY KEY AUTO_INCREMENT,
     record_date     DATE,
@@ -84,6 +97,7 @@ CREATE TABLE TrainingRecordTable (
 		REFERENCES PersonnelTable(employee_number)
         ON UPDATE CASCADE);
 
+-- Create table for storing resumes of employees stored by HR (utilizing BLOB)
 CREATE TABLE ResumeTable (
     resume_number   INTEGER PRIMARY KEY AUTO_INCREMENT,
     resume_date     DATE,
@@ -93,6 +107,9 @@ CREATE TABLE ResumeTable (
 		REFERENCES PersonnelTable(employee_number)
         ON UPDATE CASCADE);
 
+-- Create referential relationship table to tie personnel to groups
+-- This method allows multiple users per group and multiple groups
+-- Per user.
 CREATE TABLE UserGroupTable (
     group_number    INTEGER NOT NULL,
     employee_number INTEGER NOT NULL,
@@ -102,5 +119,6 @@ CREATE TABLE UserGroupTable (
 	FOREIGN KEY (group_number)
 		REFERENCES GroupTable(group_number)
         ON UPDATE CASCADE ON DELETE CASCADE);
-	
+
+-- Add default user sysadmin
 INSERT INTO UserLoginTable (user_number, user_name, access_level, password_hash, last_login) VALUES (0, "sysadmin", 3, "$2y$10$adPqrmjo64L6E9jF6WfY8OauqXjt62gn31lDkK4UlAzbYPoUac9Xy", NULL);
